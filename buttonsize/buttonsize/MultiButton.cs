6e4ed5e933dbc7ff90e6929785d334d83100ac07
Paul Johnson <paul@snuberry-software.co.uk>
2015-03-16 20:02:39 +0000

    initial import

---
+++ buttonsize/buttonsize/MultiButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace buttonsize
+{
+    public class MultiButton : ContentPage
+    {
+        public MultiButton()
+        {
+            var third = App.ScreenSize.Width / 3;
+            var quarter = App.ScreenSize.Width / 4;
+            var fifth = App.ScreenSize.Width / 5;
+
+            var thirdButton = new Button()
+            {
+                Text = "1/3",
+                TextColor = Color.Blue,
+                WidthRequest = third
+            };
+            var quarterdButton = new Button()
+            {
+                Text = "1/4",
+                TextColor = Color.Red,
+                WidthRequest = quarter
+            };
+            var fifthButton = new Button()
+            {
+                Text = "1/5",
+                TextColor = Color.Green,
+                WidthRequest = fifth
+            };
+            
+            Content = new StackLayout
+            { 
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Children =
+                {
+                    thirdButton, quarterdButton, fifthButton
+                }
+            };
+        }
+    }
+}
+
+
---
+++ buttonsize/iOS/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+namespace buttonsize.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+
+            LoadApplication(new App());
+            App.ScreenSize = new Size(UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height);
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
+
